76a4c481c873d94807092862ad88171280f7c4fd
Gavin <gk2008wong@gmail.com>
2024-08-15 19:32:31 -0400

    Added download functionality and removed some unnecessary code within the skel.py file

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,18 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory, send_file
 import tempfile
 from skeleton_generation.multiple_overlay import skeletonize
 import os
+import uuid
 
 app = Flask(__name__)
+
+file_dict = {}
+
+# Determine the base directory
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Set the result folder relative to the base directory
+app.config['RESULT_FOLDER'] = os.path.join(base_dir, 'output_path')
 
 @app.route('/upload', methods=["POST"])
 def upload():
@@ -11,22 +20,45 @@ def upload():
 
         file = request.files['file']
 
+        file_unique_id = str(uuid.uuid4())
+
         if not file:
             return jsonify({"msg": "File Not Accessible!"}), 400
 
-        file_name, _ = os.path.splitext(file.filename)
+        file_name, file_ext = os.path.splitext(file.filename)
+        skeleton_fil_name = f"{file_name}-skeleton{file_ext}"
 
         with tempfile.NamedTemporaryFile(delete=False) as input_file:
             input_path = input_file.name
             file.save(input_path)
-
-        output_path = r"backend\output_path"
         
-        skeletonize(input_path, output_path, file_name)
+        skeletonize(input_path, app.config['RESULT_FOLDER'], skeleton_fil_name)
+
+        file_dict[file_unique_id] = {"skeleton": skeleton_fil_name}
 
         os.remove(input_path)
     
-        return jsonify({"msg": "Video Uploaded Succesfully!"}), 200
+        return jsonify({"msg": "Video Uploaded Succesfully!", "id": file_unique_id}), 200
+    
+@app.route('/download/<unique_id>', methods=["GET"])
+def download(unique_id):
+
+    print(f"Received unique_id: {unique_id}")
+
+    if unique_id in file_dict:
+        file_name = file_dict[unique_id]["skeleton"]
+        file_path = os.path.join(app.config['RESULT_FOLDER'], file_name)
+
+        print(f"Looking for file: {file_path}")
+
+        if os.path.exists(file_path):
+            print("hi")
+            return send_from_directory(app.config['RESULT_FOLDER'], file_name, as_attachment=True, mimetype='video/mp4')
+        else:
+            return jsonify({"msg": "Video Not Found!"}), 404      
+    else:
+        return jsonify({"msg": "ID Not Found!"}), 404
+    
     
 if __name__ == "__main__":
     app.run(debug=True)
